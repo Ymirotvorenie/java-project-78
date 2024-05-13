bfff53bef37cdfaacaf58b5c237cf32bc6384a5d
ymirotvorenie <karoline.navarra@gmail.com>
2024-05-13 18:28:39 +0700

    add NumberSchema

---
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -0,0 +1,18 @@
+package hexlet.code.schemas;
+
+public class NumberSchema extends BaseSchema<Integer> {
+    public NumberSchema required() {
+        super.required();
+        return this;
+    }
+
+    public NumberSchema positive() {
+        rules.put("positive", n -> n > 0);
+        return this;
+    }
+
+    public NumberSchema range(int begin, int end) {
+        rules.put("range", n -> n >= begin && n <= end);
+        return this;
+    }
+}
